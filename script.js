c668f95dd08641143b4262eadfa9158f661e529e
Aayushrohilla <aayushrohilla3@gmail.com>
2024-12-07 11:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  //your code here
-	 let result = '';
+  const romanNumerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+  
+  let result = '';
   
   for (let i = 0; i < romanNumerals.length; i++) {
     const [symbol, value] = romanNumerals[i];
@@ -24,10 +29,17 @@ function convertToRoman(num) {
   return result;
 }
 
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-console.log(convertToRoman(36));
+readline.question('Enter a number: ', (num) => {
+  const roman = convertToRoman(parseInt(num));
+  console.log(`Roman numeral: ${roman}`);
+  readline.close();
+});
+
 
 
 
